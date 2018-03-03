b3d383cfb8c7c01b3079d237bbc1211e312ff27b
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-03-03 20:18:01 +0100

    add sellIn algorithm field to aged brie

--- src/main/java/com/gildedrose/items/AgedBrie.java
+++ src/main/java/com/gildedrose/items/AgedBrie.java
@@ -2,16 +2,21 @@ package com.gildedrose.items;
 
 import com.gildedrose.InventoryItemStrategy;
 import com.gildedrose.Item;
+import com.gildedrose.algorithm.Algorithm;
+import com.gildedrose.algorithm.Decrease;
 
 public class AgedBrie extends InventoryItemStrategy {
 
+    private final Algorithm sellInAlgorithm;
+
     public AgedBrie(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
+        sellInAlgorithm = new Decrease();
     }
 
     @Override
     protected void changeSellIn(Item item) {
-        item.sellIn--;
+        item.sellIn = sellInAlgorithm.calculate(item.sellIn);
     }
 
     @Override

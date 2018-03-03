de3cf67fc057278715e76f9cdb5d7bde704b3caf
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-03-03 19:04:40 +0100

    reduce InventoryItem api surface

--- src/main/java/com/gildedrose/InventoryItem.java
+++ src/main/java/com/gildedrose/InventoryItem.java
@@ -1,13 +1,6 @@
 package com.gildedrose;
 
 public interface InventoryItem {
-    
-    int getSellIn();
-
-    int getQuality();
 
     void endOfDay();
-
-    @Override
-    String toString();
 }
--- src/test/java/com/gildedrose/items/AgedBrieTest.java
+++ src/test/java/com/gildedrose/items/AgedBrieTest.java
@@ -1,6 +1,5 @@
 package com.gildedrose.items;
 
-import com.gildedrose.InventoryItem;
 import org.junit.Test;
 
 import static com.gildedrose.Inventory.item;
@@ -10,14 +9,14 @@ public class AgedBrieTest {
 
     @Test
     public void should_increase_in_quality_as_it_gets_older() {
-        InventoryItem item = item("Aged Brie", 10, 10);
+        AgedBrie item = (AgedBrie) item("Aged Brie", 10, 10);
         item.endOfDay();
         assertEquals(11, item.getQuality());
     }
 
     @Test
     public void should_decrease_number_of_days_to_sell_at_the_end_of_day() {
-        InventoryItem item = item("Aged Brie", 10, 10);
+        AgedBrie item = (AgedBrie) item("Aged Brie", 10, 10);
         item.endOfDay();
         assertEquals(9, item.getSellIn());
     }

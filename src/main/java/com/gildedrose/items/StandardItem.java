5fbd1f983af03419485f0702c3f36ed6d01fa0ac
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-03-03 20:14:25 +0100

    add sellIn algorithm field to standard item

--- src/main/java/com/gildedrose/items/StandardItem.java
+++ src/main/java/com/gildedrose/items/StandardItem.java
@@ -2,17 +2,21 @@ package com.gildedrose.items;
 
 import com.gildedrose.InventoryItemStrategy;
 import com.gildedrose.Item;
+import com.gildedrose.algorithm.Algorithm;
 import com.gildedrose.algorithm.Decrease;
 
 public class StandardItem extends InventoryItemStrategy {
 
+    private Algorithm sellInAlgorithm;
+
     public StandardItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
+        sellInAlgorithm = new Decrease();
     }
 
     @Override
     protected void changeSellIn(Item item) {
-        item.sellIn = new Decrease().calculate(item.sellIn);
+        item.sellIn = sellInAlgorithm.calculate(item.sellIn);
     }
 
     @Override

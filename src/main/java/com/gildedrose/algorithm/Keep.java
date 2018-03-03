1a71fe84e11f33f8ea28dd4b0594b0f9e7fed432
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-03-03 20:20:42 +0100

    add keep algorithm and change sulfuras to use it

---
+++ src/main/java/com/gildedrose/algorithm/Keep.java
@@ -0,0 +1,8 @@
+package com.gildedrose.algorithm;
+
+public class Keep implements Algorithm {
+
+    public int calculate(int i) {
+        return i;
+    }
+}
--- src/main/java/com/gildedrose/items/Sulfuras.java
+++ src/main/java/com/gildedrose/items/Sulfuras.java
@@ -2,16 +2,22 @@ package com.gildedrose.items;
 
 import com.gildedrose.InventoryItemStrategy;
 import com.gildedrose.Item;
+import com.gildedrose.algorithm.Algorithm;
+import com.gildedrose.algorithm.Decrease;
+import com.gildedrose.algorithm.Keep;
 
 public class Sulfuras extends InventoryItemStrategy {
 
+    private final Algorithm sellInAlgorithm;
+
     public Sulfuras(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
+        sellInAlgorithm = new Keep();
     }
 
     @Override
     protected void changeSellIn(Item item) {
-
+        item.sellIn = sellInAlgorithm.calculate(item.sellIn);
     }
 
     @Override

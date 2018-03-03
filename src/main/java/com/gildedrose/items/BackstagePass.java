49a44307e5dcaf96de7e4c54d8e80e543ce0f52d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-03-03 20:22:13 +0100

    add sellin algorithm field to backstage pass

--- src/main/java/com/gildedrose/items/BackstagePass.java
+++ src/main/java/com/gildedrose/items/BackstagePass.java
@@ -2,16 +2,21 @@ package com.gildedrose.items;
 
 import com.gildedrose.InventoryItemStrategy;
 import com.gildedrose.Item;
+import com.gildedrose.algorithm.Algorithm;
+import com.gildedrose.algorithm.Decrease;
 
 public class BackstagePass extends InventoryItemStrategy {
 
+    private final Algorithm sellInAlgorithm;
+
     public BackstagePass(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
+        sellInAlgorithm = new Decrease();
     }
 
     @Override
     protected void changeSellIn(Item item) {
-        item.sellIn--;
+        item.sellIn = sellInAlgorithm.calculate(item.sellIn);
     }
 
     @Override
